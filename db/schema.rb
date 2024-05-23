0cd4ce30b2d4912f3a2db26271d433fa81eb944a
david30-maker <davidokpah396@gmail.com>
2024-05-23 15:50:29 +0100

    create model car and reservation

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_05_23_144916) do
+  create_table "cars", force: :cascade do |t|
+    t.string "make"
+    t.string "modle"
+    t.integer "year"
+    t.decimal "price_per_dat"
+    t.boolean "availability"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "user_name"
+    t.string "user_email"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+  end
+
+  add_foreign_key "reservations", "cars"
+end
